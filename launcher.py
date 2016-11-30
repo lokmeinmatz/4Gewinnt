ee343880ef3c8132515456f25acf3f2c4ceb3330
lokmeinmatz <matze.kind@web.de>
2016-11-30 11:35:32 +0100

    Launcher funktioniert, muss 4gewinnt class starten

--- launcher.py
+++ launcher.py
@@ -1,6 +1,29 @@
-import tkinter
+import tkinter as tk
 
 
-launcher = tkinter.Tk()
+
+launcher = tk.Tk()
+
+feldgrLabel = tk.Label(launcher, text="Feldgröße")
+feldgrLabel.pack(side=tk.TOP)
+
+breite = tk.Entry(launcher)
+breite.pack(side = tk.LEFT)
+
+xLabel = tk.Label(launcher, text="x")
+xLabel.pack(side=tk.LEFT)
+
+hoehe = tk.Entry(launcher)
+hoehe.pack(side = tk.LEFT)
+
+def startGame():
+    x = breite.get()
+    y = hoehe.get()
+    print("start Spiel mit Feldgroesse: "+x+" x " + y)
+    
+    #hier dann das Objekt 4gewinnt erstellen
+
+startbutton = tk.Button(launcher, text="Start", command=startGame)
+startbutton.pack()
 
 launcher.mainloop()
