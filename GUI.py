b35bfcb1330683fb1a4e4fc97115106c485f159a
lokmeinmatz <matze.kind@web.de>
2016-12-04 18:28:10 +0100

    Skalierung & Buttons

--- GUI.py
+++ GUI.py
@@ -4,12 +4,15 @@ from functools import partial
 
 class gui:
 
+    def setScale(self, scale):
+        self.scale = scale
+
     def setCoin(self, x, y, value):
         self.fieldarray[y][x] = value
-        dx = (x + 1) * 50 - 4
-        dy = (y + 1) * 50 - 4
-        x = x*50+4
-        y = y*50+4
+        dx = (x + 1) * 50*self.scale - 4
+        dy = (y + 1) * 50*self.scale - 4
+        x = x*50*self.scale+4
+        y = y*50*self.scale+4
 
         print(x, y, dx, dy)
 
@@ -27,13 +30,14 @@ class gui:
 
     def initField(self, fieldarray):
         self.fieldarray = fieldarray
-        self.canvas.create_rectangle(0, 0, self.breite*50, self.leange*50, fill="blue")
+        self.canvas.create_rectangle(0, 0, self.breite*50*self.scale, self.leange*50*self.scale, fill="blue")
 
 
     def addCoinCallback(self, x):
         print("Ein Chip wurde in Reihe "+str(x)+" hinzugefuegt")
 
-    def __init__(self, x, y, spieler1name, spieler1farbe, spieler2name, spieler2farbe):
+    def __init__(self, x, y, spieler1name, spieler1farbe, spieler2name, spieler2farbe, scale):
+        self.setScale(scale)
         self.breite = x
         self.leange = y
         self.sp1color = spieler1farbe
@@ -43,18 +47,22 @@ class gui:
 
         self.spieleristdrann = tk.Label(self.window, text="Spieler "+spieler1name+" ist drann")
         self.spieleristdrann.pack(side=tk.TOP)
-        self.buttonframe = tk.Frame(self.window)
+        self.buttonframe = tk.Frame(self.window, width=x*50*self.scale)
         #die add buttons
         for i in range(x):
             #partial laesst der Funktion Argumente hinzufuegen
-            button = tk.Button(self.buttonframe, text="+", command=partial(self.addCoinCallback, i))
-            #button.pack(side=tk.LEFT)
-            button.place(x=x*50, y=20, width=50, anchor=tk.NE)
+            tempbuttonframe = tk.Frame(self.buttonframe, width=50*scale, height=20)
+            tempbuttonframe.pack(side=tk.LEFT)
+            tempbuttonframe.pack_propagate(False)   
 
-        self.buttonframe.pack()
+            button = tk.Button(tempbuttonframe, text="+", command=partial(self.addCoinCallback, i))
+            button.pack()
+            #button.place(bordermode=tk.OUTSIDE, x=x*50*self.scale, y=0, height=20, width=)
+
+        self.buttonframe.pack(pady=10)
 
         #spielfeld
-        self.canvas = tk.Canvas(self.window, width=x*50, height=y*50)
+        self.canvas = tk.Canvas(self.window, width=x*50*self.scale, height=y*50*self.scale)
         self.canvas.pack(side=tk.BOTTOM)
 
         #malt das blaue HG-Feld und fuellt einen Array mit den Chips(circle-positions)
@@ -71,4 +79,4 @@ class gui:
 
         self.window.mainloop()
 
-window = gui(10, 5, "Peter","yellow", "Kevin", "green")
+window = gui(10, 5, spieler1name="Peter",spieler1farbe="yellow", spieler2name="Kevin", spieler2farbe="green", scale=3)
