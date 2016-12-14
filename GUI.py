024bd3e5dc90f25cc4a62bb42e7beca54f601e3b
lokmeinmatz <matze.kind@web.de>
2016-12-14 11:06:05 +0100

    GUI mit Komments

--- GUI.py
+++ GUI.py
@@ -4,11 +4,23 @@ from functools import partial
 
 class gui:
 
+    #die "normale" Funktion fuer das Coin einfuegen mit dieser Funktion setzen
+    # gui.setCoinCommand(befehl ohne klammern)
+    
+    def setCoinCommand(self, function):
+        self.command = function
+
     def setScale(self, scale):
         self.scale = scale
 
     def setCoin(self, x, y, value):
-        self.fieldarray[y][x] = value
+        #Benutzung:
+        #x und y sind die Koordinaten in dem Feld welches verändert werden soll
+        #abfragen sind nicht sinnvoll, die Martix hier ist nur fuer die Darstellung
+    
+        #Value: 0 malt Feld weiß, 1 malt Spielerfarbe 1 und 2 Spielerfarbe 2
+        
+        
         dx = (x + 1) * 50*self.scale - 4
         dy = (y + 1) * 50*self.scale - 4
         x = x*50*self.scale+4
@@ -28,15 +40,23 @@ class gui:
 
         self.canvas.create_oval(x, y, dx, dy, fill=fill_color)
 
-    def initField(self, fieldarray):
-        self.fieldarray = fieldarray
+    def initField(self):
+    # malt den Hintergrund. wird von init aufgerufen, daher nicht noetig selbest aufzurufen
         self.canvas.create_rectangle(0, 0, self.breite*50*self.scale, self.leange*50*self.scale, fill="blue")
 
 
     def addCoinCallback(self, x):
         print("Ein Chip wurde in Reihe "+str(x)+" hinzugefuegt")
+        
+        #sollte die hinzufuegefunktion des normalen Programmes aufrufen
+        # x ist die Reihe, mit partial - Funktion werte uebergeben
+        if(self.command != None):
+            self.command()
 
     def __init__(self, x, y, spieler1name, spieler1farbe, spieler2name, spieler2farbe, scale):
+    
+        self.command = None
+        
         self.setScale(scale)
         self.breite = x
         self.leange = y
@@ -66,9 +86,7 @@ class gui:
         self.canvas.pack(side=tk.BOTTOM)
 
         #malt das blaue HG-Feld und fuellt einen Array mit den Chips(circle-positions)
-        field = [[0 for i in range(x)]for j in range(y)]
-
-        self.initField(field)
+        self.initField()
 
         for xx in range(0, x):
             for yy in range(0, y):
@@ -79,4 +97,8 @@ class gui:
 
         self.window.mainloop()
 
-window = gui(10, 5, spieler1name="Peter",spieler1farbe="yellow", spieler2name="Kevin", spieler2farbe="green", scale=3)
+        
+#Konstruktor, (Breite, Laenge, spielername 1, farbe spieler 1(auf Englisch), spielername 2, spieler 2 Farbe, Groese)
+window = gui(10, 5, spieler1name="Peter",spieler1farbe="yellow", spieler2name="Kevin", spieler2farbe="green", scale=2)
+
+window.setCoinCommand(Null)
